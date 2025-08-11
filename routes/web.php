a6e81c406f5aba9b15caac40e501763ff25d06bf
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-08-11 14:06:53 -0300

    003

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    echo "Welcome to Laravel Studies - Foryify!";
+    return view('home');
 })->middleware(['auth']);
-
-
-// Route::view('/login', 'login')->name('login');
